fa7ebf9e84f06cafe3c4df86a6ad9ded43e30bc2
Michael Wong <40144792+glowmachine@users.noreply.github.com>
2026-09-14 14:21:06 +1000

    chore: update comments

--- src/components/List/buildLink.ts
+++ src/components/List/buildLink.ts
@@ -3,33 +3,32 @@ import type { RowData } from "../../types/RowData";
 
 export default function buildLink(key: keyof RowData, row: Row): string | undefined {
     switch (key) {
-        // 
         case 'bioguide': return `https://bioguide.congress.gov/search/bio/${row.bioguide}`
-        //thomas.gov was superceeded by congress.gov, some links now using bioguide instead of thomas ID, name string inconsident
+        //thomas.gov superceeded by congress.gov, name string inconsident and some now using bioguide instead of thomas ID
         //case 'thomas': return `https://www.congress.gov/member/${[row.first, row.last].join(' ').toLowerCase().replace(' ', '-')}/${row.thomas}`
         //case 'lis':
         //case 'fec':
         case 'govtrack': return `https://www.govtrack.us/congress/members/${row.govtrack}`
         case 'opensecrets': return `https://www.opensecrets.org/profiles/${[row.first, row.last].join(' ').toLowerCase().replace(' ', '-')}/us_congress/summary?mpid=${row.opensecrets}`
-        //unable to search
+        //unable to search votesmart
         // case 'votesmart':
         case 'icpsr': return `https://voteview.com/person/${row.icpsr}`;
         //cspan path appears to be depreciated
         // case 'cspan': return `https://www.c-span.org/person/${row.cspan}`;
         case 'wikipedia': return `https://wikipedia.org/wiki/${row.wikipedia}`;
         case 'ballotpedia': return `https://ballotpedia.org/${row.ballotpedia}`;
-        // unable to search
+        // unable to search maplight
         // case 'maplight':
         case 'house_history': return `https://history.house.gov/People/Detail/${row.house_history}`;
         case 'wikidata': return `https://www.wikidata.org/wiki/${row.wikidata}`;
-        // unable to search
+        // unable to search google
         // case 'google_entity_id':
 
         case 'url': return `${row.url}`;
         case 'contact_form': return `${row.contact_form}`;
 
         case 'twitter': return `https://twitter.com/${row.twitter}`;
-        //twitter_id path appears to be depreciated 
+        //twitter_id path appears to be depreciated
         // case 'twitter_id': return `https://twitter.com/${row.twitter_id}`;
         case 'facebook': return `https://facebook.com/${row.facebook}`;
         case 'youtube': return `https://youtube.com/user/${row.youtube}`;
@@ -41,4 +40,3 @@ export default function buildLink(key: keyof RowData, row: Row): string | undefi
         default: return undefined;
     }
 }
-
