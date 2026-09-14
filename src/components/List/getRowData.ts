12256f0f682265f25b2d4b602668833393cbc2ff
Michael Wong <40144792+glowmachine@users.noreply.github.com>
2026-09-14 14:37:16 +1000

    refactor: reduce repetition

--- src/components/List/getRowData.ts
+++ src/components/List/getRowData.ts
@@ -1,75 +1,74 @@
 import type { Legislator } from "../../types/LegislatorSchema";
-import type { LeadershipRole } from "../../types/LegislatorCurrentSchema";
 import type { RowData } from "../../types/RowData";
-
-function getLeadershipRole(terms: LeadershipRole[] | undefined): string | undefined {
-    if (!terms || terms[terms.length - 1].end) return;
-    return terms[terms.length - 1].title;
-}
 
 export default function getRowData(data: Legislator[]): RowData[] {
     const selectedData = data.map(member => {
-        const currentTerm = member.currentData.terms[member.currentData.terms.length - 1];
+        const name = member.currentData.name;
+        const id = member.currentData.id;
+        const bio = member.currentData.bio;
+        const currentTerm = member.currentData.terms.at(-1);
+        const currentLeadership = member.currentData.leadership_roles?.at(-1);
+        const social = member.socialData?.social;
 
         return {
             name: `${member.currentData.name.first} ${member.currentData.name.last}`,
 
-            bioguide: member.currentData.id.bioguide,
-            thomas: member.currentData.id.thomas,
-            lis: member.currentData.id.lis,
-            govtrack: String(member.currentData.id.govtrack),
-            opensecrets: member.currentData.id.opensecrets,
-            votesmart: member.currentData.id.votesmart ? String(member.currentData.id.votesmart) : undefined,
+            bioguide: id.bioguide,
+            thomas: id.thomas,
+            lis: id.lis,
+            govtrack: id.govtrack,
+            opensecrets: id.opensecrets,
+            votesmart: id.votesmart,
             // fec: string[],
-            cspan: member.currentData.id.cspan ? String(member.currentData.id.cspan) : undefined,
-            wikipedia: member.currentData.id.wikipedia,
-            house_history: member.currentData.id.house_history ? String(member.currentData.id.house_history) : undefined,
-            ballotpedia: member.currentData.id.ballotpedia,
-            maplight: member.currentData.id.maplight ? String(member.currentData.id.maplight) : undefined,
-            icpsr: member.currentData.id.icpsr ? String(member.currentData.id.icpsr) : undefined,
-            wikidata: member.currentData.id.wikidata,
-            google_entity_id: member.currentData.id.google_entity_id,
-            pictorial: member.currentData.id.pictorial ? String(member.currentData.id.pictorial) : undefined,
+            cspan: id.cspan,
+            wikipedia: id.wikipedia,
+            house_history: id.house_history,
+            ballotpedia: id.ballotpedia,
+            maplight: id.maplight,
+            icpsr: id.icpsr,
+            wikidata: id.wikidata,
+            google_entity_id: id.google_entity_id,
+            pictorial: id.pictorial,
 
-            first: member.currentData.name.first,
-            last: member.currentData.name.last,
-            official_full: member.currentData.name.official_full,
-            middle: member.currentData.name.middle,
-            nickname: member.currentData.name.nickname,
-            suffix: member.currentData.name.suffix,
+            first: name.first,
+            last: name.last,
+            official_full: name.official_full,
+            middle: name.middle,
+            nickname: name.nickname,
+            suffix: name.suffix,
 
-            birthday: member.currentData.bio.birthday,
-            gender: member.currentData.bio.gender,
+            birthday: bio.birthday,
+            gender: bio.gender,
 
-            type: currentTerm.type,
-            start: currentTerm.start,
-            end: currentTerm.end,
-            state: currentTerm.state,
-            district: currentTerm.district,
-            party: currentTerm.party,
-            class: currentTerm.class,
-            url: currentTerm.url,
-            address: currentTerm.address,
-            phone: currentTerm.phone,
-            fax: currentTerm.fax,
-            contact_form: currentTerm.contact_form,
-            office: currentTerm.office,
-            state_rank: currentTerm.state_rank,
-            rss_url: currentTerm.rss_url,
-            caucus: currentTerm.caucus,
-            how: currentTerm.how,
-            "end-type": currentTerm["end-type"],
+            type: currentTerm?.type,
+            start: currentTerm?.start,
+            end: currentTerm?.end,
+            state: currentTerm?.state,
+            district: currentTerm?.district,
+            party: currentTerm?.party,
+            class: currentTerm?.class,
+            url: currentTerm?.url,
+            address: currentTerm?.address,
+            phone: currentTerm?.phone,
+            fax: currentTerm?.fax,
+            contact_form: currentTerm?.contact_form,
+            office: currentTerm?.office,
+            state_rank: currentTerm?.state_rank,
+            rss_url: currentTerm?.rss_url,
+            caucus: currentTerm?.caucus,
+            how: currentTerm?.how,
+            "end-type": currentTerm?.["end-type"],
             // party_affiliations: currentTerm.party_affiliations,
-            leadership: getLeadershipRole(member.currentData.leadership_roles),
+            leadership: currentLeadership?.title,
 
-            twitter: member.socialData?.social.twitter,
-            twitter_id: member.socialData?.social.twitter_id,
-            facebook: member.socialData?.social.facebook,
-            youtube: member.socialData?.social.youtube,
-            youtube_id: member.socialData?.social.youtube_id,
-            instagram: member.socialData?.social.instagram,
-            instagram_id: member.socialData?.social.instagram_id,
-            mastodon: member.socialData?.social.mastodon,
+            twitter: social?.twitter,
+            twitter_id: social?.twitter_id,
+            facebook: social?.facebook,
+            youtube: social?.youtube,
+            youtube_id: social?.youtube_id,
+            instagram: social?.instagram,
+            instagram_id: social?.instagram_id,
+            mastodon: social?.mastodon,
         };
     })
     return selectedData;
